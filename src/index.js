922de9d97a2c6758d2d25502be6880ed89872586
MarcoB <marco@befund.com>
2023-01-11 20:04:07 +0100

    transition to v3.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import {BaseProvider, LightTheme} from 'baseui'
+import {Provider as StyletronProvider} from "styletron-react"
+import {Client as Styletron} from "styletron-engine-atomic"
+import './Utilities/theme.css'
+import '../../../GitHub/palm_v3/src/App.css'
+import Apps from "./App"
+
+const engine = new Styletron()
+function App() { return <Apps /> }
+
+const rootElement = document.getElementById("root")
+
+ReactDOM.render(
+    <StyletronProvider value={engine}>
+        <BaseProvider theme={LightTheme}>
+            <App />
+        </BaseProvider>
+    </StyletronProvider>,
+   rootElement
+)
